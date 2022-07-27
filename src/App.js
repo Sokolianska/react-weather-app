d263c1d14357dae88d12c51856b556843e78a90d
Olga Sokolianska <99847102+Sokolianska@users.noreply.github.com>
2022-07-27 20:20:11 +0200

    added the GitHub repo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 
 function App() {
   return <div className="App">
-      Weather App
+   <h1> Weather App</h1>
+    <footer>
+      This poject was coded by <a href="https://github.com/Sokolianska" target="_blank">Olha Sokolianska</a> and is 
+    <a href="https://github.com/Sokolianska/react-weather-app" target="_blank"> open-sorced on GitHub
+      </a>
+      </footer>
     </div>;
 }
 
