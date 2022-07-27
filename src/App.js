0922b9a44d489516f910ab3359f1338c33b3c401
Olga Sokolianska <99847102+Sokolianska@users.noreply.github.com>
2022-07-27 20:46:52 +0200

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 
 function App() {
-  return <div className="App">
+  return (<div className="App">
+    <div className="container">
    <h1> Weather App</h1>
     <footer>
-      This poject was coded by <a href="https://github.com/Sokolianska" target ="_black" rel="noopener">Olha Sokolianska</a> and is 
-    <a href="https://github.com/Sokolianska/react-weather-app" target ="_black" rel="noopener"> open-sorced on GitHub
+      This poject was coded by <a href="https://github.com/Sokolianska" rel="noopener">Olha Sokolianska</a> and is 
+    <a href="https://github.com/Sokolianska/react-weather-app" rel="noopener"> open-sorced on GitHub
       </a>
       </footer>
-    </div>;
+      </div>
+    </div>);
 }
 
 export default App;
