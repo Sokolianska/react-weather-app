ce73c08409a8c4088929e60d126dec57348f85e1
Olga Sokolianska <99847102+Sokolianska@users.noreply.github.com>
2022-07-27 20:27:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return <div className="App">
    <h1> Weather App</h1>
     <footer>
-      This poject was coded by <a href="https://github.com/Sokolianska" target="_blank">Olha Sokolianska</a> and is 
-    <a href="https://github.com/Sokolianska/react-weather-app" target="_blank"> open-sorced on GitHub
+      This poject was coded by <a href="https://github.com/Sokolianska" target ="_black" rel="noopener">Olha Sokolianska</a> and is 
+    <a href="https://github.com/Sokolianska/react-weather-app" target ="_black" rel="noopener"> open-sorced on GitHub
       </a>
       </footer>
     </div>;
