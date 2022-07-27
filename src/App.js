15526d3371c146b5563c56276a023c9867054a30
Olga Sokolianska <99847102+Sokolianska@users.noreply.github.com>
2022-07-27 23:32:26 +0200

    added elements of Weather App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 function App() {
   return (<div className="App">
     <div className="container">
-   <h1> Weather App</h1>
+      <Weather />
     <footer>
       This poject was coded by <a href="https://github.com/Sokolianska" rel="noopener">Olha Sokolianska</a> and is 
     <a href="https://github.com/Sokolianska/react-weather-app" rel="noopener"> open-sorced on GitHub
---
+++ src/Weather.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+    return (<div className="Weather">
+        <form>
+            <div className="row">
+                <div className="col-9">
+                    <input type="search" placeholder="Enter a city..." className="form-control" />
+                </div>
+                <div className="col-3">
+                    <input type="submit" value="Search" className="btn btn-primary" />
+                    </div>
+                </div>
+        </form>
+        <h1>Dnipro</h1>   
+        <ul>
+            <li>Wednesday 07:00</li>
+            <li>Mostly Cloudy</li>
+        </ul>
+        <div className="row">
+            <div className="col-6">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Mostly cloudy" />
+              6°C
+            </div>
+            <div className="col-6">
+                <ul>
+<li>Опади: 0%</li>
+<li>Вологість: 50%</li>
+<li>Вітер: 16 км/год</li>
+                </ul>
+            </div>
+        </div>
+      </div>);
+}
